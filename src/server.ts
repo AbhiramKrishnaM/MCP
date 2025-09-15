40c13fe1dcb589327b09c021df6d882607cb9496
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2025-09-16 01:29:56 +0530

    first tool

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,83 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import z from "zod";
+import fs from "node:fs/promises";
 
 const server = new McpServer({
-    name: 'mcp',
-    version: '1.0.0',
-    capabilities: {
-        resources:{},
-        tools: {},
-        prompts:{}
-    },
-})
+  name: "mcp",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+    prompts: {},
+  },
+});
 
+server.tool(
+  "create-user",
+  "Create a new user in the database",
+  {
+    name: z.string(),
+    email: z.string().email(),
+    address: z.string(),
+    phone: z.string(),
+  },
+  {
+    title: "Create User",
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true,
+  },
+  async (params) => {
+    try {
+      const id = await createUser(params);
+      return {
+        content: [
+          {
+            type: "text",
+            text: `User created with id ${id}`,
+          },
+        ],
+      };
+    } catch (error) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Error creating user",
+          },
+        ],
+      };
+    }
+  }
+);
+
+type User = {
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+};
+
+async function createUser(user: User) {
+  const users = await import("./data/users.json", {
+    with: { type: "json" },
+  }).then((module) => module.default);
+
+  const id = users.length + 1;
+
+  users.push({ ...user, id });
+
+  await fs.writeFile("./data/users.json", JSON.stringify(users, null, 2));
+
+  return id;
+}
 
 async function main() {
-    const transport = new StdioServerTransport()
+  const transport = new StdioServerTransport();
 
-    await server.connect(transport)
+  await server.connect(transport);
 }
 
 main();
