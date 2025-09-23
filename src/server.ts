10c045ca8ea53fdb947c12e09db78018bfb135d4
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2025-09-24 01:06:33 +0530

    fix for create user

--- src/server.ts
+++ src/server.ts
@@ -67,9 +67,9 @@ async function createUser(user: User) {
 
   const id = users.length + 1;
 
-  users.push({ ...user, id });
+  users.push({ id, ...user });
 
-  await fs.writeFile("./data/users.json", JSON.stringify(users, null, 2));
+  await fs.writeFile("./src/data/users.json", JSON.stringify(users, null, 2));
 
   return id;
 }
